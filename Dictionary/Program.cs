7e3e41b218714b702553071f559ae4a447390c66
kemal <kemal@LAPTOP-J49F5T9Q>
2021-01-23 18:10:35 +0300

    Ödev

---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int,string> cities = new MyDictionary<int,string>();
+
+
+            cities.Add(68, "Aksaray");
+            cities.Add(70, "Karaman");
+            cities.Add(26, "Eskişehir");
+            cities.Show();
+
+        }
+    }
+}
